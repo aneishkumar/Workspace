521dd8b2dcf594df9211f3da61fd4657b6d8b45c
aneish <aneish>
2017-12-16 17:58:57 +0530

    This is my commit

---
+++ Registration/WebContent/Index.jsp
@@ -0,0 +1,23 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Registration Form</title>
+</head>
+<body>
+<h1>Registration Form</h1>
+<form method="post" action="/Login"><br><br>
+<table>
+<tr><td>Name:</td><td><input type="text" name="Name"></td></tr>
+<tr><td>Age:</td><td><input type="text" name="Age"></td></tr>
+<tr><td>Address:</td><td><input type="text" name="Address"></td></tr>
+<tr><td>UserName:</td><td><input type="text" name="Username"></td></tr>
+<tr><td>Password:</td><td><input type="Password" name="Password"></td></tr>
+<tr><td></td><td><input type="Submit" Value="Submit"></td></tr>
+
+</table>
+</form>
+</body>
+</html>
---
+++ Registration/src/Checklogin.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Checklogin
+ */
+@WebServlet("/Checklogin")
+public class Checklogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Checklogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String Password1=request.getParameter("Password1");
+		String Username1=request.getParameter("Username1");
+		if(Username.equals(Username1))
+		{
+			response.sendRedirect("Information.jsp");
+		}else
+		{
+			response.sendRedirect("Wrong.jsp");
+		}
+	}
+
+}
